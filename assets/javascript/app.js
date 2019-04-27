4da27686df0197ec2915e3d38fb15de4ea3d5ad3
James <jimbomoso@gmail.com>
2019-04-27 08:28:08 -0400

    started javascript add ready function assigned variables set up generic question object

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+    var questionCounter = 0;
+
+    var answerTimeout = 5000;
+
+    var correct = 0;
+    var wrong = 0;
+    var skipped = 0;
+
+    var userAnswers = [];
+
+    var questions = [
+
+        {
+            Question: "....?",
+            Choices: ["a", "b", "c"],
+            correctAnswer: [0]
+        }
+    ]
+
+}
